bd0fe200d980d163a6edf777611a997b358bd8f1
sanskritigupta18 <96648416+sanskritigupta18@users.noreply.github.com>
2025-01-03 22:46:40 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "https://google-drive-backend-3p6k.onrender.com";
 
 export const userEndpoints = {
     REGISTER_USER: BASE_URL + "/user/register",
